818a527c8659a0444a4e7b0784861df828ea771e
Guil192 <132094759+Guil192@users.noreply.github.com>
2023-05-29 01:12:39 -0300

    final

--- truck-way/js/frete.js
+++ truck-way/js/frete.js
@@ -1,8 +1,12 @@
 function calcularFrete() {
     const km = parseFloat(document.getElementById("km").value);
-    const custoPorKm = 50; // inserir aqui o custo por km, ou seja, o custo da diesel por quilometro rodado
-    const custoFixo = 200; // inserir aqui o custo fixo, ou seja despesas
+    const custoPorKm = 7.10; // inserir aqui o custo por km, ou seja, o custo da diesel por quilometro rodado
+    const custoFixo = 100; // inserir aqui o custo fixo, ou seja despesas
     const total = km * custoPorKm + custoFixo;
-
-    document.getElementById("total").innerHTML = `Total do frete: R$ ${total.toFixed(2)}`;
+    if (!total) {
+    document.getElementById("total").innerHTML = `Digite um valor.`
+    }
+    else {
+    document.getElementById("total").innerHTML = `Total do frete: R$ ${total.toFixed(2)}`
+    };
 }

faebe4193f3a90c72a16c50595a4669a8712657e
Silva-carlos1 <132786335+Silva-carlos1@users.noreply.github.com>
2023-06-03 09:35:02 -0300

    senha atual de login: etec@1234

--- truck-way/js/login.js
+++ truck-way/js/login.js
@@ -3,7 +3,7 @@ function logar() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if (login == "admin" && senha == "admin") {
+    if (login == "admin" && senha == "etec@1234") {
         location.href = "funcionario.html";
     } else {
         alert('Usuario ou senha incorretos');
